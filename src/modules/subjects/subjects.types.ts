59da26494df111a39a392c86cae552a306f08d96
Paul Smith <psmith@magna.co.uk>
2026-03-26 13:23:47 +0000

    feat: create subject query and types

---
+++ src/modules/subjects/subjects.types.ts
@@ -0,0 +1,10 @@
+export type createSubjectRequest = {
+  name: string;
+};
+
+export type subjectResponse = {
+  id: string;
+  name: string;
+};
+
+export type createSubjectResponse = subjectResponse;
